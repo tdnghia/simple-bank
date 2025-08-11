f7fb9aceb584199275a04e42bdb7b3a02202faff
Nghia Tran <tdnghia101@gmail.com>
2025-08-11 15:59:40 +0700

    Add random functions for unit tests

--- util/random.go
+++ util/random.go
@@ -14,7 +14,7 @@ func init() {
 
 // Generate a random integer between min and max
 func RandomInt(min, max int64) int64 {
-	return min * rand.Int63n(max-min+1)
+	return min + rand.Int63n(max-min+1)
 }
 
 // Generate a random string of length n
