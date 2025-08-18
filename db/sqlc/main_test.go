7036018951ed653f042cbde5e6db3aba06c6fe9f
Nghia Tran <tdnghia101@gmail.com>
2025-08-18 19:21:09 +0700

    Load .env using viper

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,20 +7,21 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
+	"github.com/tdnghia/simple-bank/util"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-)
-
 func TestMain(m *testing.M) {
 	var err error
 
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../../")
+	if err != nil {
+		log.Fatal("cannot load config", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to database", err)
 	}
