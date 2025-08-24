446a3c583f5f8cfd1166e125649deea81cfb5388
Nghia Tran <tdnghia101@gmail.com>
2025-08-24 14:49:21 +0700

    API: Create User

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 func createRandomAccount(t *testing.T) Account {
-	user := createRandomUser()
+	user := createRandomUser(t)
 
 	arg := CreateAccountParams{
 		Owner:    user.Username,
---
+++ util/password_test.go
@@ -0,0 +1,28 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestPassword(t *testing.T) {
+	password := RandomString(6)
+
+	hashedPassowrd1, err := HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassowrd1)
+
+	err = checkPassword(password, hashedPassowrd1)
+	require.NoError(t, err)
+
+	wrongPassword := RandomString(6)
+	err = checkPassword(wrongPassword, hashedPassowrd1)
+	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+
+	hashedPassowrd2, err := HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassowrd1)
+	require.NotEqual(t, hashedPassowrd1, hashedPassowrd2)
+}
