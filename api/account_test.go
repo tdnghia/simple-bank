da7ad24f5b515d8e68146f021fd819202ebf5700
Nghia Tran <tdnghia101@gmail.com>
2025-08-25 15:51:33 +0700

    API: Login User

--- api/account_test.go
+++ api/account_test.go
@@ -83,7 +83,7 @@ func TestGetAccountAPI(t *testing.T) {
 			tc.buildStubs(store)
 
 			// start test server and send request
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			url := fmt.Sprintf("/accounts/%d", tc.accountID)
@@ -169,7 +169,7 @@ func TestCreateAccountAPI(t *testing.T) {
 			tc.buildStubs(store, account)
 
 			// start test server and send request
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			data, err := json.Marshal(tc.arg)
